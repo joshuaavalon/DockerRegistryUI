8813d3b720c574ff07f66e444b72f8df34757506
Joshua Avalon <joshuaavalon@gmail.com>
2018-06-06 11:45:30 +0800

    Enable some config to pass by environment variable

--- server/app/config.py
+++ server/app/config.py
@@ -1,5 +1,9 @@
-DEBUG = False
+import os
 
-REGISTRY_URL = "http://localhost:5000/"
-REGISTRY_VERIFY_SSL = True
-REGISTRY_AUTHORIZATION = None
+DEBUG = bool(os.environ.get("DEBUG", False))
+
+REGISTRY_URL = os.environ.get("REGISTRY_URL", "http://localhost:5000/")
+
+REGISTRY_VERIFY_SSL = bool(os.environ.get("REGISTRY_VERIFY_SSL", True))
+
+REGISTRY_AUTHORIZATION = os.environ.get("REGISTRY_AUTHORIZATION", None)
